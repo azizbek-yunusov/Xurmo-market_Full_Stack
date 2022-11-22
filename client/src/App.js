48640ff00ce8a5eeec3ae75ab87f382621bc4e31
azizbekyunusov <azizbekaliyevich@gmail.com>
2022-11-22 18:01:45 +0300

    react verstka

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AddProduct from "./pages/AddProduct";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-500">hello MERN</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/add" element={<AddProduct />} />
+      </Routes>
+    </>
   );
 }
 
